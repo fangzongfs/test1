3d4a3c83e63a74cec677b623cdb9857866ac0afe
fangzongfs <91427114+fangzongfs@users.noreply.github.com>
2022-05-05 17:56:49 +0800

    F4外置测试

---
+++ cmake-build-debug/CMakeFiles/test1.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj.d
@@ -0,0 +1,19 @@
+CMakeFiles/test1.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj: \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\portable\GCC\ARM_CM4F\port.c \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/FreeRTOS.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdint.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_default_types.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\features.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_newlib_version.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_intsup.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_stdint.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Core\Inc/FreeRTOSConfig.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/projdefs.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/portable.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/deprecated_definitions.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\portable\GCC\ARM_CM4F/portmacro.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/mpu_wrappers.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/task.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/list.h
---
+++ cmake-build-debug/CMakeFiles/test1.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/test1.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj: \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\timers.c \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdlib.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_ansi.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\newlib.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_newlib_version.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\config.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\features.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\reent.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_ansi.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_types.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_types.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_default_types.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\lock.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\cdefs.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\stdlib.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\alloca.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/FreeRTOS.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdint.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_intsup.h \
+ e:\stmicroelectronics\gcc-arm-none-eabi-10.3-2021.10-win32\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_stdint.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Core\Inc/FreeRTOSConfig.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/projdefs.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/portable.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/deprecated_definitions.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\portable\GCC\ARM_CM4F/portmacro.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/mpu_wrappers.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/task.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/list.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/queue.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/task.h \
+ D:\CLionProjects\FreeRTOS\STM32F411\test1\Middlewares\Third_Party\FreeRTOS\Source\include/timers.h
